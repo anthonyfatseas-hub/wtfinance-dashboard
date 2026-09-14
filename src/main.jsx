3ae7e4462bb3e3e37470c35321599dba6bf18ae1
anthonyfatseas-hub <anthonyfatseas@gmail.com>
2026-09-14 12:33:58 +0100

    Show live integration status in dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Pipeline from './Pipeline.jsx';
+import IntegrationStatus from './IntegrationStatus.jsx';
 import './storage-shim.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Pipeline />
+    <IntegrationStatus />
   </React.StrictMode>
 );
